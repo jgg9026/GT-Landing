90d0bdb32056397068b5133245d43df1a276134b
Jonathan <jgg.9026@gmail.com>
2015-10-21 02:39:21 -0500

    trabajando en responsive, animacion sede bar completa

--- source/javascripts/page1.js
+++ source/javascripts/page1.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
-  var menu_item=$(".warpper .page1 .menu .sidebar").css("width");
-
+  var menu_item=$(".sidebar").width();
+  console.log(menu_item);
+  console.log($(".sidebar").css("width"));
+  console.log($("body").css("width"));
+  var ten=$("body").css("width"); 
+  ten= parseInt(ten, 10);
+  ten =ten/10;
+  console.log(ten);
+  console.log($(".sidebar").css("width"));
+  var ancho=0;
   $(".wrapper .page1 .burguer .burguer_button").click(function(){
-    console.log("hola");
-    console.log(menu_item);
-      switch ($(".wrapper .page1 .menu").css("width")){
-        case "10%":
-          $(".warpper .page1 .menu").animate({'width': '0px'}, 400);
+      var size=$(".sidebar").css("width");
+      console.log(size);
+      switch ($(".sidebar").css("width")){
+        case '0px':
+          $(".menu").animate({'width': '10%'}, 250);
+          $(".wrapper .page1 .burguer .burguer_button").animate({'left':ten+'px'}, 400);
+          $(".burguer").animate({'left': '8%'}, 250);
           console.log("hola");
           break;
-        case "0%":
-          $(".warpper .page1 .menu").animate({'width': '10%'}, 400);
+        case size:
+          $(".menu").animate({'width': '0px'}, 250);
+          $(".burguer").animate({'left': '1%'},250);
+
           break;
       }
   });
